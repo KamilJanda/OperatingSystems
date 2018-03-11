d357dcf8a4951742eee5580c06789f34e60fdd8b
„Kamil” <kamiljanda@yahoo.pl>
2018-03-11 23:39:57 +0100

    parser protype

--- lab1/main.c
+++ lab1/main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dynamicblocks.h"
 
-int main() {
+void createArray();
+
+int main( int argc, char *argv[])  {
+
+    if(argc<5)
+    {
+        printf("Too few arguments\n");
+        printf("example call: ./main [size of array] [size of blocks] [d/s] [operations]\n");
+        exit(0);
+    }
+
+    int arraySize = (int)argv[1];
+    int blockSize = (int)argv[2];
+    char allocationType = argv[3];
+
+    
+
+
 
     ArrayOfBlocks* test = create_array_of_blocks(2,3);
 
 
     printf("%d",test-> array[1][2]);
     return 0;
+}
+
+void createArray()
+{
+
 }
